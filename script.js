7409d867bd59e840d2ba5e58741db33a468d0de2
vYareskoDev <viaresko@gmail.com>
2023-05-26 16:21:02 +0300

    created burger nav

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const hamburger = document.querySelector("#hamburger");
-const hamburgerMenu = document.querySelector("#hambuger-menu");
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+const headerNav = document.querySelector("header nav");
 
 
 hamburger.addEventListener("click", () => {
-    hamburgerMenu.classList.remove("hidden");
+    if(hamburger.dataset.state !== "default") {
+        hamburger.src = "./icons/white-theme-close-burger.svg";
+        hamburger.dataset.state = "default";
+    }else {
+        hamburger.src = "./icons/white-theme-burger.svg";
+        hamburger.dataset.state = "opened";
+    }
+    hamburgerMenu.classList.toggle("hidden");
+});
+
+window.addEventListener("resize", () => {
+    if(document.body.clientWidth <= 1000) {
+        headerNav.classList.add('hidden');
+        hamburger.classList.remove('hidden');
+    }else {
+        if(headerNav.classList.contains('hidden')) {
+            headerNav.classList.remove('hidden');
+            hamburger.classList.add('hidden')
+        }
+    }
 })
